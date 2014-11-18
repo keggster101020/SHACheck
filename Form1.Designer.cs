4c80423cd8016187de372fa257a2a920c739791c
keggster101020 <keeganshudy@gmail.com>
2014-11-17 22:16:23 -0500

    Added SHA 256 support

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -36,6 +36,9 @@
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.metroLabel1 = new MetroFramework.Controls.MetroLabel();
             this.SHAtoCheck = new MetroFramework.Controls.MetroTextBox();
+            this.check = new MetroFramework.Controls.MetroButton();
+            this.metroLabel2 = new MetroFramework.Controls.MetroLabel();
+            this.sha256 = new MetroFramework.Controls.MetroLabel();
             this.SuspendLayout();
             // 
             // labelSHA
@@ -59,7 +62,7 @@
             // metroTextBox1
             // 
             this.metroTextBox1.Lines = new string[] {
-        "Application"};
+        "Browse for a file"};
             this.metroTextBox1.Location = new System.Drawing.Point(208, 106);
             this.metroTextBox1.MaxLength = 32767;
             this.metroTextBox1.Name = "metroTextBox1";
@@ -68,7 +71,7 @@
             this.metroTextBox1.SelectedText = "";
             this.metroTextBox1.Size = new System.Drawing.Size(291, 23);
             this.metroTextBox1.TabIndex = 2;
-            this.metroTextBox1.Text = "Application";
+            this.metroTextBox1.Text = "Browse for a file";
             this.metroTextBox1.UseSelectable = true;
             // 
             // browseButton
@@ -84,6 +87,7 @@
             // SHA1
             // 
             this.SHA1.AutoSize = true;
+            this.SHA1.LabelMode = MetroFramework.Controls.MetroLabelMode.Selectable;
             this.SHA1.Location = new System.Drawing.Point(208, 169);
             this.SHA1.Name = "SHA1";
             this.SHA1.Size = new System.Drawing.Size(103, 19);
@@ -98,7 +102,7 @@
             // metroLabel1
             // 
             this.metroLabel1.AutoSize = true;
-            this.metroLabel1.Location = new System.Drawing.Point(107, 224);
+            this.metroLabel1.Location = new System.Drawing.Point(107, 271);
             this.metroLabel1.Name = "metroLabel1";
             this.metroLabel1.Size = new System.Drawing.Size(96, 19);
             this.metroLabel1.TabIndex = 5;
@@ -107,8 +111,8 @@
             // SHAtoCheck
             // 
             this.SHAtoCheck.Lines = new string[] {
-        "Input SHA1"};
-            this.SHAtoCheck.Location = new System.Drawing.Point(209, 224);
+        "Input SHA value"};
+            this.SHAtoCheck.Location = new System.Drawing.Point(208, 267);
             this.SHAtoCheck.MaxLength = 32767;
             this.SHAtoCheck.Name = "SHAtoCheck";
             this.SHAtoCheck.PasswordChar = '\0';
@@ -116,20 +120,53 @@
             this.SHAtoCheck.SelectedText = "";
             this.SHAtoCheck.Size = new System.Drawing.Size(291, 23);
             this.SHAtoCheck.TabIndex = 6;
-            this.SHAtoCheck.Text = "Input SHA1";
+            this.SHAtoCheck.Text = "Input SHA value";
             this.SHAtoCheck.UseSelectable = true;
+            // 
+            // check
+            // 
+            this.check.Location = new System.Drawing.Point(505, 267);
+            this.check.Name = "check";
+            this.check.Size = new System.Drawing.Size(75, 23);
+            this.check.TabIndex = 7;
+            this.check.Text = "Check";
+            this.check.UseSelectable = true;
+            this.check.Click += new System.EventHandler(this.check_Click);
+            // 
+            // metroLabel2
+            // 
+            this.metroLabel2.AutoSize = true;
+            this.metroLabel2.Location = new System.Drawing.Point(107, 214);
+            this.metroLabel2.Name = "metroLabel2";
+            this.metroLabel2.Size = new System.Drawing.Size(58, 19);
+            this.metroLabel2.TabIndex = 0;
+            this.metroLabel2.Text = "SHA256:";
+            // 
+            // sha256
+            // 
+            this.sha256.AutoSize = true;
+            this.sha256.LabelMode = MetroFramework.Controls.MetroLabelMode.Selectable;
+            this.sha256.Location = new System.Drawing.Point(208, 214);
+            this.sha256.Name = "sha256";
+            this.sha256.Size = new System.Drawing.Size(103, 19);
+            this.sha256.TabIndex = 4;
+            this.sha256.Text = "PLACE HOLDER";
+            this.sha256.Visible = false;
             // 
             // CheckSHA
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(726, 388);
+            this.ClientSize = new System.Drawing.Size(778, 388);
+            this.Controls.Add(this.check);
             this.Controls.Add(this.SHAtoCheck);
             this.Controls.Add(this.metroLabel1);
+            this.Controls.Add(this.sha256);
             this.Controls.Add(this.SHA1);
             this.Controls.Add(this.browseButton);
             this.Controls.Add(this.metroTextBox1);
             this.Controls.Add(this.browseLabel);
+            this.Controls.Add(this.metroLabel2);
             this.Controls.Add(this.labelSHA);
             this.Name = "CheckSHA";
             this.Text = "Check SHA";
@@ -149,6 +186,9 @@
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
         private MetroFramework.Controls.MetroLabel metroLabel1;
         private MetroFramework.Controls.MetroTextBox SHAtoCheck;
+        private MetroFramework.Controls.MetroButton check;
+        private MetroFramework.Controls.MetroLabel metroLabel2;
+        private MetroFramework.Controls.MetroLabel sha256;
 
 
     }
--- Form1.cs
+++ Form1.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.IO;
+using System.Security;
 using System.Windows.Forms;
 using MetroFramework.Forms;
 using System.Security.Cryptography;
@@ -27,45 +28,73 @@ namespace SHACheck
         private void browseButton_Click(object sender, EventArgs e)
         {
             SHA1.Text = "";
+            sha256.Text = "";
+            SHAtoCheck.Text = "Input SHA value";
             DialogResult dr = openFileDialog1.ShowDialog();
 
             if (dr == DialogResult.OK)
             {
-                String file = openFileDialog1.FileName;
-                FileStream stream = File.OpenRead(file);
+                String file = openFileDialog1.FileName; //get filepath
+                FileStream stream256 = File.OpenRead(file); //open file for read only
                 metroTextBox1.Text = file;
-
-                using (SHA1Managed sha1 = new SHA1Managed())
-                {
-                    BufferedStream bs = new BufferedStream(stream);
-                    byte[] hash = sha1.ComputeHash(bs);
-                    StringBuilder formatted = new StringBuilder(2 * hash.Length);
-                    foreach (byte b in hash)
-                    {
-                        formatted.AppendFormat("{0:X2}", b);
-                    }
-                    SHA1.Text = formatted.ToString();
-                    SHA1.Visible = true;
-                    
-                    if (!(SHAtoCheck.Text == "" || SHAtoCheck == null || SHAtoCheck.Text == "Input SHA1"))
-                    {
-                        //MetroMessageBox.Show(this, "Enter a SHA1 to check against", "ALERT", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Asterisk);
-                        String checkagainst = SHAtoCheck.Text;
-                        if (formatted.ToString().Equals(checkagainst, StringComparison.InvariantCultureIgnoreCase))
-                        {
-                            MetroMessageBox.Show(this, "The SHA1 checks out ok", "Checked", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                        }
-                        else
-                        {
-                            MetroMessageBox.Show(this, "The SHA1 doesn't match!", "ALERT", MessageBoxButtons.OK, MessageBoxIcon.Warning);
-                            
-                        }
-                    }
-                }
+                String SHA256 = computeSHA256(stream256);
+                stream256.Close(); //otherwise the data will be written twice to the stream
                 
+                FileStream stream = File.OpenRead(file);
+                String SHA = computeSHA(stream);
+                stream.Close();
+                
+                SHA1.Text = SHA;
+                sha256.Text = SHA256;
+                SHA1.Visible = true;
+                sha256.Visible = true;
             }
 
         }
 
+        private void check_Click(object sender, EventArgs e)
+        {
+            //making sure the check box has some validation
+            if (SHAtoCheck.Text == "" || SHAtoCheck.Text == null || SHAtoCheck.Text == "Input SHA value"
+                || SHA1.Text == "" || sha256.Text == "" || SHA1.Text == "PLACE HOLDER" || sha256.Text == "PLACE HOLDER")
+            {
+                MetroMessageBox.Show(this, "Enter a SHA to check against", "Check is Empty of invalid", MessageBoxButtons.OK, MessageBoxIcon.None);
+            }
+            else
+            {
+                String computedSHA = SHA1.Text;
+                String computedSHA256 = sha256.Text;
+                String checkSHA = SHAtoCheck.Text;
+                //does an equalsIgnoreCase
+                if (computedSHA.Equals(checkSHA, StringComparison.InvariantCultureIgnoreCase) || computedSHA256.Equals(checkSHA,StringComparison.InvariantCultureIgnoreCase))
+                {
+                    MetroMessageBox.Show(this, "The SHA's match", "Match", MessageBoxButtons.OK, MessageBoxIcon.Question);
+                }
+                else
+                {
+                    MetroMessageBox.Show(this, "The SHA's don't match", "ALERT", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+            }
+        }
+
+        private String computeSHA(FileStream stream)
+        {
+            SHA1Managed sha1 = new SHA1Managed();
+            BufferedStream bs = new BufferedStream(stream);
+            byte[] hash = sha1.ComputeHash(bs);
+            String hold = BitConverter.ToString(hash).Replace("-", String.Empty);
+            sha1.Clear();
+            return hold;
+        }
+
+        private String computeSHA256(FileStream stream)
+        {
+            SHA256Managed sha256 = new SHA256Managed();
+            BufferedStream bs = new BufferedStream(stream);
+            byte[] hash = sha256.ComputeHash(bs);
+            String holder = BitConverter.ToString(hash).Replace("-", String.Empty);
+            sha256.Clear();
+            return holder;
+        }
     }
 }
